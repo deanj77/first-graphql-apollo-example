5eb5887327ca50d96fe3ac6f4a7a08a524e904f3
deanj77 <deanj7798@gmail.com>
2023-05-06 06:45:16 +0330

    first try graphql

---
+++ FakeData.js
@@ -0,0 +1,32 @@
+const UserList = [
+  {
+    id: 1,
+    name: "John",
+    username: "john",
+    age: 20,
+    nationality: "canada",
+  },
+  {
+    id: 2,
+    name: "Pedro",
+    username: "pedro",
+    age: 21,
+    nationality: "iran",
+  },
+  {
+    id: 3,
+    name: "Mohammad",
+    username: "mohammad",
+    age: 22,
+    nationality: "us",
+  },
+  {
+    id: 4,
+    name: "Rose",
+    username: "rose",
+    age: 25,
+    nationality: "iq",
+  },
+];
+
+module.exports = { UserList };
